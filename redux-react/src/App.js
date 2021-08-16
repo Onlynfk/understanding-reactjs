f77e19fb7377113b58c0fc2ffff5d3f325774224
OnlyGod Ovbije <onlygodovbije@gmail.com>
2021-08-16 12:25:25 +0100

    updated

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -1,15 +1,76 @@
-import {useSelector} from  'react-redux'
+import  {Fragment, useEffect, useState} from 'react'
+import {useSelector, useDispatch} from  'react-redux'
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {uiActions} from './store/ui-slice';
+import Notification from './components/UI/Notification';
 
+let isInitial = true;
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisibile)
+  const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    const sendCartdata = async() =>{
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      ); 
+    const response = await fetch('https://chefburger-f2e12-default-rtdb.firebaseio.com/cart.json',
+    {
+      method: 'PUT',
+      body:JSON.stringify(cart)
+    });
+    if(!response.ok){
+      throw new Error('Sending cart data failed');  
+    }
+
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!',
+      })
+    );
+  };
+
+
+  if (isInitial) {
+    isInitial = false;
+    return;
+  }
+
+  // sendCartdata().catch((error) =>{
+  //   dispatch(uiActions.showNotification({
+  //     status: 'error',
+  //     title:'Errro...',
+  //     message: 'Sending cart data failed!',
+
+  //   }))
+  // })
+    
+  }, [cart, dispatch])  
+
   return (
-    <Layout>
+    <Fragment>
+      <Notification
+        status = {notification.status}
+        title = {notification.title}
+        message = {notification.message}
+
+      />
+      <Layout>
       {showCart && <Cart /> }
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
